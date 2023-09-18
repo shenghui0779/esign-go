0a951d9ffc3a27385261685dcc7d5e0ea9b0290a
shenghui <shenghui0779@gmail.com>
2023-09-18 11:29:44 +0800

    Update option

--- client.go
+++ client.go
@@ -293,8 +293,8 @@ func (c *Client) Verify(header http.Header, body []byte) error {
 // Option 自定义设置项
 type Option func(c *Client)
 
-// WithClient 设置自定义 HTTP Client
-func WithClient(cli *http.Client) Option {
+// WithHttpCli 设置自定义 HTTP Client
+func WithHttpCli(cli *http.Client) Option {
 	return func(c *Client) {
 		c.httpCli = NewHTTPClient(cli)
 	}
